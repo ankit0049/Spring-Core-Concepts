8808cf22c5bb4ccc0f8b8925ae037b66943080ad
ankit_049 <ankitrajputji00@gmail.com>
2025-02-24 01:19:53 +0530

    [Ankit Rajput] Add. Added Feature to log custom messages using predfined class

---
+++ springconcepts/src/main/java/com/demo/springconcepts/controller/GreetingController.java
@@ -0,0 +1,28 @@
+package com.demo.springconcepts.controller;
+
+import com.demo.springconcepts.service.GreetingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/greetings")
+public class GreetingController {
+
+    // Create a Logger Instance to log custom messages
+    private static final Logger logger = LoggerFactory.getLogger(GreetingController.class);
+
+    @Autowired
+    GreetingService greetingService;
+    @GetMapping
+    public String getGreeting() {
+        logger.info("Controller Layer Invoked Here");
+        return greetingService.getGreetings();
+    }
+
+    @GetMapping("/{name}")
+    public String getPersonalizedGreeting(@PathVariable String name) {
+        return "Hello, " + name + "! Welcome to Spring Boot!";
+    }
+}  
