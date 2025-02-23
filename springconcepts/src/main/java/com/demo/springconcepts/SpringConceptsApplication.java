731d3e01d4f8076b0afb6193430db5db7056ea1b
ankit_049 <ankitrajputji00@gmail.com>
2025-02-24 01:30:43 +0530

    [Ankit Rajput] Add. Implemented Dependecy Injection Using Employee and Department bean and Application Context

--- springconcepts/src/main/java/com/demo/springconcepts/SpringConceptsApplication.java
+++ springconcepts/src/main/java/com/demo/springconcepts/SpringConceptsApplication.java
@@ -1,13 +1,20 @@
 package com.demo.springconcepts;
 
+import com.demo.springconcepts.beans.EmployeeBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringConceptsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringConceptsApplication.class, args);
+
+	    ApplicationContext context = SpringApplication.run(SpringConceptsApplication.class, args);
+
+	    // Retrieving EmployeeBean from ApplicationContext
+	    EmployeeBean employee = context.getBean(EmployeeBean.class);
+	    System.out.println(" Employee Details: " + employee);
 	}
 
 }
